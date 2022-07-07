578bfa8860b140fdc4e1bd07cca879dbc4144a3f
伍卓睿 <wuzhuorui@gmail.com>
2022-07-08 01:14:11 +0800

    feat:db using online atlas

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 module.exports = {
-    MONGODB: "mongodb://127.0.0.1:27017/amt_menu_db",
+    // mongodb+srv://hawkWu:<password>@cluster0.2tu54yq.mongodb.net/test
+    // MONGODB: "mongodb://127.0.0.1:27017/amt_menu_db",
+    MONGODB: "mongodb+srv://hawkWu:wzr644392839@cluster0.2tu54yq.mongodb.net/test",
+    // mongodb+srv://hawkWu:wzr644392839@cluster0.2tu54yq.mongodb.net/test
     SecretAccessKey: '123',
     Expiration: '1h',
 }
